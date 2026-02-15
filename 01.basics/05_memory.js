72652a65cd2e8b36e1a2251c2142deef7338a1ca
ArtAmit <artamit27@gmail.com>
2026-02-15 14:40:18 +0000

    memory

---
+++ 01.basics/05_memory.js
@@ -0,0 +1,16 @@
+//stack (primitive) 
+// Heap (non primitive)
+let myyoutubename="artamit.com"
+let anothername=myyoutubename
+anothername="chaiaurcode"
+console.log(myyoutubename);
+console.log(anothername);
+ let userone={
+    email:"user2google.com",
+    upi:"user@ybl"
+ }
+ let usertwo=userone
+ console.log(usertwo);
+ usertwo.email="amit@gmail"
+ console.log(userone);
+ console.log(usertwo);
